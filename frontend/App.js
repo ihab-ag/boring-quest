0f75e4eb3b9f1c99e907b6c3187dc1c6c38de700
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-27 23:49:49 +0300

    <frontend> config: fonts loaded

--- frontend/App.js
+++ frontend/App.js
@@ -1,9 +1,24 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native'
+import { useFonts } from 'expo-font'
 
 export default function App() {
+
+  // Load fonts
+  const [fontsLoaded] = useFonts({
+    'Inter': require('./assets/fonts/static/Inter-Regular.ttf'),
+    'Inter-Medium': require('./assets/fonts/static/Inter-Medium.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/static/Inter-SemiBold.ttf'),
+    'Inter-Bold': require('./assets/fonts/static/Inter-Bold.ttf'),
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+
   return (
-    <View >
-      
+    <View>
+
     </View>
   );
 }

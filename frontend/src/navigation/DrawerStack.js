0097752263deb6ade8b3d3ce7eb383f7a9e56f1a
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-29 00:53:49 +0300

    <frontend> feat: drawer customization

--- frontend/src/navigation/DrawerStack.js
+++ frontend/src/navigation/DrawerStack.js
@@ -1,16 +1,45 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
+import CustomDrawer from "../components/CustomDrawer";
 import QuestsScreen from "../screens/QuestsScreen";
+import { Octicons } from '@expo/vector-icons';
 
+const screens = {
+    'Quests': QuestsScreen,
+}
 
 export const Drawer = createDrawerNavigator()
 
 export const DrawerNavigator = () => {
     return (
-    <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Quests" useLegacyImplementation screenOptions={{headerShown: false}}>
-            <Drawer.Screen name="Quests" component={QuestsScreen} />
-        </Drawer.Navigator>
-    </NavigationContainer>)
+        <NavigationContainer>
+            <Drawer.Navigator
+                initialRouteName="Quests"
+                drawerContent={props => <CustomDrawer {...props} />}
+                useLegacyImplementation
+                screenOptions={{
+                    headerShown: false,
+                    drawerIcon: () => (<Octicons name="diamond" size={24} color="white" />),
+                    drawerLabelStyle: {
+                        marginLeft: -16,
+                        color: '#fff',
+                        fontSize: 16,
+                        fontFamily: 'Inter-Medium'
+                    },
+                    drawerActiveBackgroundColor: '#073B4C',
+                    drawerStyle:{  
+                        backgroundColor: '#118AB2'
+                    },
+                    drawerItemStyle:{
+                        padding: 4,
+                    },
+                }}>
+                {
+                    Object.keys(screens).map((name)=> (
+                    <Drawer.Screen key={name} name={name} component={screens[name]} />
+                    ))
+                }
+            </Drawer.Navigator>
+        </NavigationContainer>)
 }
 

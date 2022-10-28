03ac2419e2db1d846685862a99a54f7f6bcc92bd
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-28 11:34:17 +0300

    <frontend> config: fonts added to tailwind theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': 'Inter',
+        'inter-bold': 'Inter-Bold',
+        'inter-medium': 'Inter-Medium',
+        'inter-semibold': 'Inter-SemiBold',
+      }
+    },
   },
   plugins: [],
 }

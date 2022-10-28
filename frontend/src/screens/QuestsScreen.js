aa5a7f48f00b47ce1625eefdb126442704ec7458
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-28 16:19:48 +0300

    <frontend> feat: quests screen header

--- frontend/src/screens/QuestsScreen.js
+++ frontend/src/screens/QuestsScreen.js
@@ -1,12 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import CustomHeader from '../components/CustomHeader'
 
-const QuestsScreen = () => {
-  return (
-    <View>
-      <Text>QuestsScreen</Text>
-    </View>
-  )
+const QuestsScreen = ({ navigation, route }) => {
+    return (
+        <View>
+            <CustomHeader navigation={navigation} route={route} />
+            <Text>QuestsScreen</Text>
+        </View>
+    )
 }
 
 export default QuestsScreen

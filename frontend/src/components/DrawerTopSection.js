eb2fab56ef742af5946104baf332d380777b7d3f
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-29 00:50:57 +0300

    <frontend> feat: drawer top section component

---
+++ frontend/src/components/DrawerTopSection.js
@@ -0,0 +1,19 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Avatar from './Avatar'
+
+const DrawerTopSection = () => {
+    return (
+        <View className='flex-row px-3 py-5 mb-5 bg-primary items-center justify-start gap-x-7'>
+            <View className='border-medium border-2 rounded-sm border-solid'>
+                <Avatar source={require('../../assets/test_avatars/avatar.png')} />
+            </View>
+            <View className='border-2 border-l-0 pr-3 border-solid border-medium h-20 justify-center'>
+                <Text className='font-inter-semibold text-lg text-white m-0 tracking-wider'>Username</Text>
+                <Text className='font-inter text-md text-white'>Adventurer</Text>
+            </View>
+        </View>
+    )
+}
+
+export default DrawerTopSection

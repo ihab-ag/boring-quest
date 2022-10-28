5d12ba9273090247c15992b343fd49f722889eab
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-28 16:19:20 +0300

    <frontend> feat: custom header

---
+++ frontend/src/components/CustomHeader.js
@@ -0,0 +1,16 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Feather } from '@expo/vector-icons';
+
+const CustomHeader = ({navigation,route}) => {
+  return (
+    <View className='flex-row h-16 w-full bg-primary px-6 items-center justify-between'>
+      <Text className='font-inter-semibold text-2xl text-white'>{route.name}</Text>
+      <TouchableOpacity onPress={()=> navigation.openDrawer()}>
+        <Feather name="menu" size={34} color="white" />
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default CustomHeader

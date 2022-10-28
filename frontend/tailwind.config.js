20249c12e842fbb75c27085fdad14d78f8184fba
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-28 16:17:57 +0300

    <frontend> config: theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}"],
+  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
@@ -8,6 +8,13 @@ module.exports = {
         'inter-bold': 'Inter-Bold',
         'inter-medium': 'Inter-Medium',
         'inter-semibold': 'Inter-SemiBold',
+      },
+      colors:{
+        'primary': '#118AB2',
+        'secondary': '#073B4C',
+        'easy':'#06D6A0',
+        'medium':'#FFD166',
+        'hard':'#EF476F',
       }
     },
   },

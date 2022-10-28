7262cdd43f021e6cece91a344ca9d82a854fd1f3
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-28 12:09:17 +0300

    <frontend> feat: drawer navigator

---
+++ frontend/src/navigation/DrawerStack.js
@@ -0,0 +1,13 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+
+
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigator = () => {
+    <NavigationContainer>
+        <Drawer.Navigator>
+            
+        </Drawer.Navigator>
+    </NavigationContainer>
+}

528efb46ed736b9f16adf5a780f957221354848e
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-28 12:11:31 +0300

    <frontend> feat: quests screen

---
+++ frontend/src/screens/QuestsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const QuestsScreen = () => {
+  return (
+    <View>
+      <Text>QuestsScreen</Text>
+    </View>
+  )
+}
+
+export default QuestsScreen

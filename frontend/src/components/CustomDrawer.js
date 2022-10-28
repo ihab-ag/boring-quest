b0ea6485dc9f59cac489c1ecc189797f926a0f96
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-29 00:52:19 +0300

    <frontend> feat: custom drawer component

---
+++ frontend/src/components/CustomDrawer.js
@@ -0,0 +1,25 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer'
+import { SimpleLineIcons } from '@expo/vector-icons'
+import DrawerTopSection from './DrawerTopSection'
+
+const CustomDrawer = (props) => {
+    return (
+        <>
+            <DrawerTopSection />
+            <DrawerContentScrollView {...props} >
+                <DrawerItemList {...props} />
+            </DrawerContentScrollView>
+            <DrawerItem
+                label={() => (
+                    <Text className='font-inter-medium text-hard text-lg -ml-4 p-1' >Logout</Text>)
+                }
+                icon={() => (
+                    <SimpleLineIcons name="logout" size={24} color="#EF476F" />)
+                } />
+        </>
+    )
+}
+
+export default CustomDrawer

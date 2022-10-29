9a0dfbba7effd029ee762f45ce9e0fe7e1753688
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-29 18:27:03 +0300

    <frontend> fix: avatar image is static

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,9 +1,9 @@
 import { Image } from 'react-native'
 import React from 'react'
 
-const Avatar = ({source}) => {
+const Avatar = () => {
   return (
-    <Image className='h-20 w-20' source={source} />
+    <Image className='h-20 w-20'  source={require('../../assets/test_avatars/avatar.png')} />
   )
 }
 export default Avatar

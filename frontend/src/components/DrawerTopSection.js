0ed7d67c7ea437e80ba0d27f6e38c323d69e73e0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-29 18:36:46 +0300

    <frontend> fix: avatar component removed source

--- frontend/src/components/DrawerTopSection.js
+++ frontend/src/components/DrawerTopSection.js
@@ -6,7 +6,7 @@ const DrawerTopSection = () => {
     return (
         <View className='flex-row px-3 py-5 mb-5 bg-primary items-center justify-start gap-x-7'>
             <View className='border-medium border-2 rounded-sm border-solid'>
-                <Avatar source={require('../../assets/test_avatars/avatar.png')} />
+                <Avatar/>
             </View>
             <View className='border-2 border-l-0 pr-3 border-solid border-medium h-20 justify-center'>
                 <Text className='font-inter-semibold text-lg text-white m-0 tracking-wider'>Username</Text>

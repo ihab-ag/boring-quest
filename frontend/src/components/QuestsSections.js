bb0bb22a74d390fe245bf37a6868153cebfb602d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-30 15:19:32 +0200

    <frontend> feat: quest sections

---
+++ frontend/src/components/QuestsSections.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import DefaultScreen from '../layouts/DefaultScreen'
+import QuestsSection from './QuestsSection'
+
+const QuestsSections = () => {
+
+    return (
+        <DefaultScreen>
+            <QuestsSection />
+            <QuestsSection />
+            <QuestsSection />
+        </DefaultScreen>
+    )
+}
+
+export default QuestsSections

2d72d23bd982c33abf9721a1dc96746391026bce
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-30 15:15:22 +0200

    <frontend> feat: progressbar size and border

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
 
-const ProgressBar = ({color}) => {
+const ProgressBar = ({ color }) => {
     return (
-        <View className='h-2.5 bg-white mt-0.5'>
-            <View className={`h-2.5 bg-${color} w-2/5`} />
+        <View className={`h-4 bg-white mt-0.5 border-primary border-2 rounded-sm border-solid`}>
+            <View className={`flex-1 bg-${color} w-2/5`} />
         </View>
     )
 }

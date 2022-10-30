92314993e7645428511cd84d369681f474e3a40d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-10-30 15:21:30 +0200

    <frontend> feat: questsscetions added

--- frontend/src/screens/QuestsScreen.js
+++ frontend/src/screens/QuestsScreen.js
@@ -1,15 +1,14 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import CustomHeader from '../components/CustomHeader'
 import QuestsScreenTopSection from '../components/QuestsScreenTopSection'
+import QuestsSections from '../components/QuestsSections'
 
 const QuestsScreen = ({ navigation, route }) => {
     return (
         <>
             <CustomHeader navigation={navigation} route={route} />
             <QuestsScreenTopSection />
-            <View className='flex-1 bg-screen-bg px-6'>
-            </View>
+            <QuestsSections />
         </>
     )
 }

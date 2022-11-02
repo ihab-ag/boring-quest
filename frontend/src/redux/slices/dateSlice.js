f901d794d5bf53fd912d164a674cd356f36c4677
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-02 11:37:46 +0200

    <frontend> feat: set month date slice reducer

--- frontend/src/redux/slices/dateSlice.js
+++ frontend/src/redux/slices/dateSlice.js
@@ -18,8 +18,14 @@ export const dateSlice = createSlice({
     name: 'date',
     initialState,
     reducers: {
-       
+        setMonth: (state, action) => {
+            state.month = action.payload
+            state.days = getMonthDays(state.year, state.month)
+            state.day = 1
+        },
+        
     }
 })
 
+export const { setMonth } = dateSlice.actions
 export default dateSlice.reducer

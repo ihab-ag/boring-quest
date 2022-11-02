8f69d6636048332d89bcb8d9646d35897610cd08
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-02 13:17:27 +0200

    <frontend> fix: size adjustment

--- frontend/src/components/DrawerTopSection.js
+++ frontend/src/components/DrawerTopSection.js
@@ -8,9 +8,9 @@ const DrawerTopSection = () => {
             <View className='border-medium border-2 rounded-sm border-solid'>
                 <Avatar/>
             </View>
-            <View className='border-2 border-l-0 pr-3 border-solid border-medium h-20 justify-center'>
-                <Text className='font-inter-semibold text-lg text-white m-0 tracking-wider'>Username</Text>
-                <Text className='font-inter text-md text-white'>Adventurer</Text>
+            <View className='flex-1 border-2 border-l-0 pr-3 border-solid border-medium h-20 justify-center'>
+                <Text className='font-inter-semibold text-white m-0 tracking-wider'>Username</Text>
+                <Text className='font-inter text-white'>Adventurer</Text>
             </View>
         </View>
     )

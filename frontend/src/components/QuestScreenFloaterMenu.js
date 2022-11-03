d53c45d08135807507c0e0b3c9b88c4072c251b4
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-03 22:21:34 +0200

    <frontend> feat: quest screen floater menu

--- frontend/src/components/QuestScreenFloaterMenu.js
+++ frontend/src/components/QuestScreenFloaterMenu.js
@@ -4,13 +4,21 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import FloaterMenuItem from './FloaterMenuItem';
 import FloaterMenu from '../layouts/FloaterMenu';
 
-const QuestScreenFloaterMenu = ({ menuStyle, menuItemStyle }) => {
+const QuestScreenFloaterMenu = ({ menuStyle, menuItemStyle, navigation }) => {
+
+    const navigateToQuest = () => {
+        navigation.navigate('QuestForm')
+    }
+
+    const navigateToAdventure = () => {
+        navigation.navigate('AdventureForm')
+    }
     return (
         <FloaterMenu menuStyle={menuStyle}>
-            <FloaterMenuItem  title='Adventure' menuItemStyle={menuItemStyle}>
+            <FloaterMenuItem  title='Adventure' menuItemStyle={menuItemStyle} onPress={navigateToAdventure}>
                 <MaterialCommunityIcons name="sword-cross" size={24} color="white" />
             </FloaterMenuItem>
-            <FloaterMenuItem  title='Quest' menuItemStyle={menuItemStyle}>
+            <FloaterMenuItem  title='Quest' menuItemStyle={menuItemStyle} onPress={navigateToQuest}>
                 <FontAwesome5 name="scroll" size={18} color="white" />
             </FloaterMenuItem>
         </FloaterMenu>

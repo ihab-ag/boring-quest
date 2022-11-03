4437eda00315fedd0ec5068c8d8a5dfd7ffcf25d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-03 12:59:03 +0200

    <frontend> feat: floater menu item

---
+++ frontend/src/components/FloaterMenuItem.js
@@ -0,0 +1,22 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import Animated from 'react-native-reanimated'
+
+const FloaterMenuItem = ({ menuItemStyle, children, title }) => {
+    return (
+        <TouchableOpacity
+            className='justify-center items-center mb-3'
+        >
+            <Animated.View
+                style={menuItemStyle}
+                className='bg-primary w-10 h-10 justify-center items-center mb-2'>
+                <View className='-rotate-45'>
+                    {children}
+                </View>
+            </Animated.View>
+            <Text className='text-secondary font-inter-medium text-xs'>{title}</Text>
+        </TouchableOpacity>
+    )
+}
+
+export default FloaterMenuItem

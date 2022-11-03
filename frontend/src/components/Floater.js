a76c032373e8262495bdd3bfe7050f7e28dc5611
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-03 22:29:03 +0200

    <frontend> feat: navigation

--- frontend/src/components/Floater.js
+++ frontend/src/components/Floater.js
@@ -10,18 +10,19 @@ import { DEVICE_HEIGHT } from '../constants/Dimensions';
 import QuestScreenFloaterMenu from './QuestScreenFloaterMenu';
 import DiamondIconContainer from './DiamondIconContainer';
 
-const Floater = () => {
+const Floater = ({navigation}) => {
 
+    // animation values
     const rotation = useSharedValue(45)
     const bottom = useSharedValue(-30)
     const height = useSharedValue(0)
     const opacity = useSharedValue(0)
-
+    // animation configuration
     const config = {
         duration: 500,
         easing: Easing.bezier(0.5, 0.01, 0, 1),
     }
-
+    // animatable styles
     const floaterStyle = useAnimatedStyle(() => {
         return {
             transform: [{ rotate: withTiming(rotation.value + 'deg', config) }],
@@ -40,7 +41,7 @@ const Floater = () => {
             opacity: withTiming(opacity.value, { duration: 700 })
         }
     })
-
+        // close and open floater menu
     const toggleMenu = () => {
         // change value depending on rotation value to keep values in sync
         rotation.value = rotation.value === 45 ? -270 : 45
@@ -51,7 +52,7 @@ const Floater = () => {
 
     return (
         <View className='absolute right-6 bottom-8 justify-center items-center'>
-            <QuestScreenFloaterMenu menuItemStyle={menuItemStyle} menuStyle={menuStyle} />
+            <QuestScreenFloaterMenu menuItemStyle={menuItemStyle} menuStyle={menuStyle} navigation={navigation} />
             <TouchableOpacity onPress={toggleMenu} >
                 <DiamondIconContainer style={floaterStyle} size='lg'>
                     <AntDesign name="plus" size={42} color="white" />

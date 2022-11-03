f82bbce1990c0a47176ac0b60eeda56b29042505
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-03 16:50:00 +0200

    <frontend> feat: dynamic header type

--- frontend/src/components/CustomHeader.js
+++ frontend/src/components/CustomHeader.js
@@ -1,13 +1,26 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
-const CustomHeader = ({navigation,route}) => {
+const CustomHeader = ({ navigation, route, type = '' }) => {
+
+  let action = null
+  let icon = null
+  // change navigation based on type
+  if (type === 'drawer'){
+    action = () => navigation.openDrawer()
+    icon = (<Feather name="menu" size={34} color="white" />)
+  }
+  else if (type === 'modal'){
+    action = () => navigation.goBack()
+    icon = (<AntDesign name="close" size={32} color="white" />)
+  }
   return (
     <View className='flex-row w-full bg-primary px-6 py-4 items-center justify-between'>
       <Text className='font-inter-semibold text-2xl text-white'>{route.name}</Text>
-      <TouchableOpacity onPress={()=> navigation.openDrawer()}>
-        <Feather name="menu" size={34} color="white" />
+      <TouchableOpacity onPress={action}>
+        {icon}
       </TouchableOpacity>
     </View>
   )

5fe2ac881db453997bfb9e67b12e956327e77436
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-05 22:01:02 +0200

    <frontend> feat: addventure reducer linked to store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import dateReducer from './slices/dateSlice'
+import adventureReducer from './slices/adventureSlice'
+
 export default configureStore({
     reducer: {
-        date: dateReducer
+        date: dateReducer,
+        adventure: adventureReducer,
     }
 })

1e7fd9dd3ec41978f58af0f81e4a70d8d5c5a598
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-05 13:41:39 +0200

    <frontend> faet: modal layout

--- frontend/src/modals/MonthYearPickerModal.js
+++ frontend/src/modals/MonthYearPickerModal.js
@@ -1,11 +1,12 @@
 import { View, Modal, FlatList } from 'react-native'
 import MONTHS from '../constants/Months';
-import { Item } from '../components/FlatlistPickerItem';
+import { Item } from '../components/ListPickerItem';
 import ModalHeader from '../components/ModalHeader';
 import { useSelector, useDispatch } from 'react-redux'
 import { setMonth, incrementYear, decrementYear } from '../redux/slices/dateSlice';
 import Counter from '../components/Counter';
 import { currentYear } from '../helpers/currentDate';
+import ModalLayout from '../layouts/ModalLayout';
 
 const MonthYearPickerModal = ({ modalVisible, setModalVisible }) => {
 
@@ -27,32 +28,24 @@ const MonthYearPickerModal = ({ modalVisible, setModalVisible }) => {
             />
         )
     }
-    
+
     return (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={modalVisible}
-        >
-            <View className='flex-1 flex-row justify-center items-center p-6 bg-black/50'>
-                <View className='flex-1 border-secondary border-2 rounded'>
-                    <ModalHeader onPress={() => setModalVisible(false)} title='Select Month and Year' />
-                    <View className='h-56 bg-white'>
-                        <Counter count={year}
-                            increment={() => dispatch(incrementYear())}
-                            decrement={() => dispatch(decrementYear())}
-                            decrementCondition={currentYear < year} />
-                        <FlatList
-                            data={MONTHS}
-                            renderItem={renderItem}
-                            keyExtractor={item => item.id}
-                            extraData={month}
-                            initialScrollIndex={month}
-                        />
-                    </View>
-                </View>
+        <ModalLayout modalVisible={modalVisible}>
+            <ModalHeader onPress={() => setModalVisible(false)} title='Select Month and Year' />
+            <View className='h-56 bg-white'>
+                <Counter count={year}
+                    increment={() => dispatch(incrementYear())}
+                    decrement={() => dispatch(decrementYear())}
+                    decrementCondition={currentYear < year} />
+                <FlatList
+                    data={MONTHS}
+                    renderItem={renderItem}
+                    keyExtractor={item => item.id}
+                    extraData={month}
+                    initialScrollIndex={month}
+                />
             </View>
-        </Modal>
+        </ModalLayout>
     )
 }
 

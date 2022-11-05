dfc09629d10aa23e7903fce6b2e490fc8d2454b1
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-05 13:48:31 +0200

    <frontend> feat: dropdown reusability

--- frontend/src/components/DatePickerSection.js
+++ frontend/src/components/DatePickerSection.js
@@ -6,11 +6,12 @@ import MonthYearPickerModal from '../modals/MonthYearPickerModal'
 import DaysSlider from './DaysSlider'
 import { useDispatch, useSelector } from 'react-redux'
 import { setDay } from '../redux/slices/dateSlice'
- 
+import MONTHS from '../constants/Months'
+
 
 const DatePickerSection = () => {
 
-    const { day, days } = useSelector(state => state.date)
+    const { day, days, month } = useSelector(state => state.date)
     const dispatch = useDispatch()
     const [modalVisible, setModalVisible] = useState(false)
 
@@ -18,7 +19,7 @@ const DatePickerSection = () => {
         <View className='mt-4' >
             <View className='flex-row justify-between items-center'>
                 <SectionTitleText>Your <Text className='font-inter'>Calender</Text></SectionTitleText>
-                <DropDown onPress={() => setModalVisible(true)} />
+                <DropDown onPress={() => setModalVisible(true)} value={MONTHS[month - 1].name} />
             </View>
             <DaysSlider dispatch={(item) => dispatch(setDay(item))} day={day} days={days} />
             <MonthYearPickerModal
--- frontend/src/components/DropDown.js
+++ frontend/src/components/DropDown.js
@@ -1,16 +1,13 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Entypo } from '@expo/vector-icons';
-import MONTHS from '../constants/Months';
 
-const DropDown = ({ onPress }) => {
-    const { month } = useSelector(state => state.date)
+const DropDown = ({ onPress, value }) => {
     
     return (
         <TouchableOpacity onPress={onPress}>
             <View className='px-2 border-2 border-secondary rounded flex-row justify-between items-center'>
-                <Text className='font-inter text-primary'>{MONTHS[month - 1].name}</Text>
+                <Text className='font-inter text-primary'>{value}</Text>
                 <Entypo name="chevron-small-down" size={25} color='#073B4C' />
             </View>
         </TouchableOpacity>

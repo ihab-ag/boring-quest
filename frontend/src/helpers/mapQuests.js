029b4e99a7d1832ed4b6913f9cca936b4ba848fa
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-06 11:14:33 +0200

    <frontend> feat: map quests

---
+++ frontend/src/helpers/mapQuests.js
@@ -0,0 +1,15 @@
+import QuestCard from "../components/QuestCard"
+
+const mapQuests = (quests, onPress) => {
+    return quests.map((quest, key) => (
+        <QuestCard key={key}
+            difficulty={`bg-${quest.difficulty}`}
+            title={quest.title}
+            description={quest.description}
+            onPress={() => onPress(key, quest.id && quest.id)}
+            status={quest.status}
+        />
+    ))
+}
+
+export default mapQuests

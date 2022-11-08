d1f98705c41466d97bf780326d02ed643a1a5213
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-08 15:37:32 +0200

    <frontend> feat: variants added

--- frontend/src/components/QuestCard.js
+++ frontend/src/components/QuestCard.js
@@ -4,24 +4,32 @@ import { Entypo } from '@expo/vector-icons';
 import DiamondIconContainer from './DiamondIconContainer';
 import { MaterialIcons } from '@expo/vector-icons';
 
-const QuestCard = ({ difficulty, title = 'Quest title', description = 'somethings im writing to get a sentence', onPress, status = 'in progress' }) => {
+const QuestCard = ({ difficulty, title, description, onPress, status = 'in progress' }) => {
 
-    let icon = (<Entypo name="dots-three-horizontal" size={18} color="white" />)
-    if (status === 'delete')
-        icon = (<MaterialIcons name="delete" size={16} color="white" />)
+    const icon = {
+        'delete':
+            <MaterialIcons name="delete" size={16} color="white" />,
+        'in progress':
+            <Entypo name="dots-three-horizontal" size={18} color="white" />,
+        'submitted':
+            <Entypo name="check" size={18} color="white" />,
+        'failed':
+            <MaterialIcons name="close" size={18} color="white" />
+    }
+
     return (
         <TouchableOpacity onPress={onPress}>
             <View className='border-2 border-primary bg-white px-4 flex-row justify-between mb-4 rounded'>
                 <View className='flex-row'>
                     <View className={`w-4 ${difficulty} h-full`} />
                     <View className='justify-center py-2 w-4/5 ml-2'>
-                        <Text className='text-primary font-inter-medium text-lg'>{title}</Text>
+                        <Text className='text-primary font-inter-medium text-lg capitalize'>{title}</Text>
                         <Text numberOfLines={1} className='text-passive font-inter text-xs'>{description}</Text>
                     </View>
                 </View>
                 <View className='justify-center items-center' style={{}}>
                     <DiamondIconContainer>
-                        {icon}
+                        {icon[status]}
                     </DiamondIconContainer>
                 </View>
             </View>

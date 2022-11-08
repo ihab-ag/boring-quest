99653ce7d2a459873142317206931f5cefdb1426
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-08 15:42:51 +0200

    <frotnend> fix: decrement condition removed

--- frontend/src/components/Counter.js
+++ frontend/src/components/Counter.js
@@ -2,11 +2,11 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { AntDesign } from '@expo/vector-icons';
 
-const Counter = ({ decrement, increment, count, decrementCondition = true }) => {
+const Counter = ({ decrement, increment, count }) => {
     return (
         <View className='bg-white py-4 border-b-2 mb-2 border-medium flex-row justify-around'>
-            <TouchableOpacity onPress={decrementCondition ? decrement : null}>
-                <AntDesign name="caretleft" size={24} color={decrementCondition ?'#073B4C' : '#a5a5a5'} />
+            <TouchableOpacity onPress={decrement}>
+                <AntDesign name="caretleft" size={24} color={'#073B4C'} />
             </TouchableOpacity>
             <Text className='text-lg text-primary font-inter-medium'>{count}</Text>
             <TouchableOpacity onPress={increment}>

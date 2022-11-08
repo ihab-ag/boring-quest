d0a2a23798e055a0860c1e0b3c950e553ccd933c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-08 15:41:30 +0200

    <frontend> feat: cleaned code

--- frontend/src/components/CustomHeader.js
+++ frontend/src/components/CustomHeader.js
@@ -3,24 +3,26 @@ import React from 'react'
 import { Feather } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
 
-const CustomHeader = ({ navigation, route, type = '' }) => {
 
-  let action = null
-  let icon = null
-  // change navigation based on type
-  if (type === 'drawer') {
-    action = () => navigation.openDrawer()
-    icon = (<Feather name="menu" size={34} color="white" />)
+
+const CustomHeader = ({ navigation, route, type = 'drawer', color = 'bg-primary' }) => {
+
+  const typeProps = {
+    drawer: {
+      action: () => navigation.openDrawer(),
+      icon: <Feather name="menu" size={34} color="white" />
+    },
+    modal: {
+      action: () => navigation.goBack(),
+      icon: <AntDesign name="close" size={32} color="white" />
+    }
   }
-  else if (type === 'modal') {
-    action = () => navigation.goBack()
-    icon = (<AntDesign name="close" size={32} color="white" />)
-  }
+
   return (
-    <View className='flex-row w-full bg-primary px-6 py-4 items-center justify-between'>
-      <Text className='font-inter-semibold text-2xl text-white'>{route.name}</Text>
-      <TouchableOpacity onPress={action}>
-        {icon}
+    <View className={`flex-row w-full ${color} px-6 py-4 items-center justify-between`}>
+      <Text className='font-inter-semibold text-2xl text-white capitalize'>{route.name}</Text>
+      <TouchableOpacity onPress={typeProps[type].action}>
+        {typeProps[type].icon}
       </TouchableOpacity>
     </View>
   )

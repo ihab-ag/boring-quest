5241604ea77dd2bbc0803d4a71eba025cad1476f
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-08 15:41:02 +0200

    <frontend> feat: cleaned code

--- frontend/src/components/DiamondIconContainer.js
+++ frontend/src/components/DiamondIconContainer.js
@@ -3,15 +3,16 @@ import React from 'react'
 import Animated from 'react-native-reanimated'
 
 const DiamondIconContainer = ({style={}, children, size='sm'}) => {
-    let dimensions = 'w-6 h-6'
     
-    if(size==='lg'){
-        dimensions = 'w-14 h-14'
+    const dimensions = {
+        'sm' : 'w-6 h-6',
+        'md' : 'w-10 h-10',
+        'lg' : 'w-14 h-14'
     }
     
     return (
         <Animated.View className='justify-center items-center rotate-45' style={style}>
-            <View className={`bg-primary ${dimensions} justify-center items-center`}>
+            <View className={`bg-primary ${dimensions[size]} justify-center items-center`}>
                 <View className='-rotate-45'>
                     {children}
                 </View>

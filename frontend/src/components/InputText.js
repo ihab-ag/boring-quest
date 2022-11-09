5a9c1ccc7378946bca2100a317f14cc3d5809fbf
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-09 22:35:18 +0200

    <frontend> feat: password type text input

--- frontend/src/components/InputText.js
+++ frontend/src/components/InputText.js
@@ -1,18 +1,19 @@
 import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
-const InputText = ({ placeholder, numberOfLines, onChangeText, onBlur, value }) => {
+const InputText = ({ placeholder, numberOfLines, onChangeText, onBlur, value, password = false }) => {
     return (
         <TextInput
             className='px-2 pt-1 border-2 border-secondary text-secondary rounded-sm mt-2 w-full bg-white placeholder:text-xl'
             placeholder={placeholder}
             textAlignVertical='top'
             cursorColor='#118AB2'
-            multiline={true}
+            multiline={!password}
             numberOfLines={numberOfLines}
             onChangeText={onChangeText}
             onBlur={onBlur}
             value={value}
+            secureTextEntry={password}
         />
     )
 }

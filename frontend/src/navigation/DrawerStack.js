37410244e6382a84f2d10f210d34511ed7256923
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-09 22:36:08 +0200

    <frontend> fix: naming changes

--- frontend/src/navigation/DrawerStack.js
+++ frontend/src/navigation/DrawerStack.js
@@ -2,44 +2,42 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 import CustomDrawer from "../components/CustomDrawer";
 import { Octicons } from '@expo/vector-icons';
-import QuestsNavigator from "./QuestsStack";
+import QuestsStack from "./QuestsStack";
 
 const screens = {
-    'Quests': QuestsNavigator,
+    'Quests': QuestsStack,
 }
 
-export const Drawer = createDrawerNavigator()
+export const DrawerNavigator = createDrawerNavigator()
 
-export const DrawerNavigator = () => {
+export const Drawer = () => {
     return (
-        <NavigationContainer>
-            <Drawer.Navigator
-                initialRouteName="Quests"
-                drawerContent={props => <CustomDrawer {...props} />}
-                useLegacyImplementation
-                screenOptions={{
-                    headerShown: false,
-                    drawerIcon: () => (<Octicons name="diamond" size={24} color="white" />),
-                    drawerLabelStyle: {
-                        marginLeft: -16,
-                        color: '#fff',
-                        fontSize: 16,
-                        fontFamily: 'Inter-Medium'
-                    },
-                    drawerActiveBackgroundColor: '#073B4C',
-                    drawerStyle:{  
-                        backgroundColor: '#118AB2'
-                    },
-                    drawerItemStyle:{
-                        padding: 4,
-                    },
-                }}>
-                {
-                    Object.keys(screens).map((name)=> (
+        <DrawerNavigator.Navigator
+            initialRouteName="Quests"
+            drawerContent={props => <CustomDrawer {...props} />}
+            useLegacyImplementation
+            screenOptions={{
+                headerShown: false,
+                drawerIcon: () => (<Octicons name="diamond" size={24} color="white" />),
+                drawerLabelStyle: {
+                    marginLeft: -16,
+                    color: '#fff',
+                    fontSize: 16,
+                    fontFamily: 'Inter-Medium'
+                },
+                drawerActiveBackgroundColor: '#073B4C',
+                drawerStyle: {
+                    backgroundColor: '#118AB2'
+                },
+                drawerItemStyle: {
+                    padding: 4,
+                },
+            }}>
+            {
+                Object.keys(screens).map((name) => (
                     <Drawer.Screen key={name} name={name} component={screens[name]} />
-                    ))
-                }
-            </Drawer.Navigator>
-        </NavigationContainer>)
+                ))
+            }
+        </DrawerNavigator.Navigator>)
 }
 

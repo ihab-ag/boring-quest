e597ad0c951e1b4116fd7b0ca2f9dfbbd03ad5d0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-09 22:41:54 +0200

    <frontend> feat: login stack added

--- frontend/App.js
+++ frontend/App.js
@@ -2,10 +2,12 @@ import { SafeAreaView, StatusBar, Text, View } from 'react-native'
 import { useFonts } from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect, useCallback } from 'react';
-import { DrawerNavigator } from './src/navigation/DrawerStack';
+import { Drawer } from './src/navigation/DrawerStack';
 import { Provider } from 'react-redux'
 import store from './src/redux/store';
 import MessageFloater from './src/components/MessageFloater';
+import LoginStack from './src/navigation/LoginStack';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
 
@@ -15,6 +17,7 @@ export default function App() {
     'Inter-Medium': require('./assets/fonts/static/Inter-Medium.ttf'),
     'Inter-SemiBold': require('./assets/fonts/static/Inter-SemiBold.ttf'),
     'Inter-Bold': require('./assets/fonts/static/Inter-Bold.ttf'),
+    'SpaceMono-Bold': require('./assets/fonts/static/SpaceMono-Bold.ttf')
   })
 
   useEffect(() => {
@@ -36,13 +39,16 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <DrawerNavigator />
-      <MessageFloater />
-      <View onLayout={onLayoutRootView}>
-        <SafeAreaView>
-          <StatusBar backgroundColor='#fff' barStyle='dark-content' />
-        </SafeAreaView>
-      </View>
+      <NavigationContainer>
+        {/* <Drawer /> */}
+        <LoginStack />
+        {/* <MessageFloater /> */}
+        <View onLayout={onLayoutRootView}>
+          <SafeAreaView>
+            {/* <StatusBar backgroundColor='#fff' barStyle='dark-content' /> */}
+          </SafeAreaView>
+        </View>
+      </NavigationContainer>
     </Provider>
   );
 }

bf6517f7118090780f2290097e27201139688b12
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-10 13:58:05 +0200

    <frontend> feat: login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,19 +1,29 @@
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, BackHandler } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import AnimatedBackground from '../components/AnimatedBackground'
 import LoginFormLayout from '../layouts/LoginFormLayout'
 import LoginForm from '../forms/LoginForm'
+import { useEffect } from 'react'
 
 const LoginScreen = ({ navigation, route }) => {
-
+    
+          useEffect(
+            () => {
+                navigation.addListener('beforeRemove',(e)=>{
+                    e.preventDefault()
+                })
+            }
+          ,[navigation])
+        //   not working note
+          
     return (
         <>
             <AnimatedBackground />
             <LoginFormLayout route={route}>
-                <LoginForm />
+                <LoginForm navigation={navigation} />
             </LoginFormLayout>
-            <StatusBar translucent />
+            <StatusBar backgroundColor='#fff0' barStyle='dark-content' />
         </>
     )
 }

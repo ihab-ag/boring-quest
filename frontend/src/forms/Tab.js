67444e1ff81b78c10bc516681337fc329c5841ad
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-10 13:55:18 +0200

    <frontend> feat: tab

---
+++ frontend/src/forms/Tab.js
@@ -0,0 +1,17 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+
+const Tab = ({ onPress, title, selected=false }) => {
+
+    return (
+        <View className='flex-1'>
+        <TouchableOpacity
+            onPress={onPress}
+            className={`border-2 justify-center items-center ${ selected ? 'border-white bg-primary' : 'bg-white border-primary'} py-2 rounded`} >
+            <Text className={`font-inter-semibold text-base ${ selected ? 'text-white' : 'text-primary'}`}>{title}</Text>
+        </TouchableOpacity>
+        </View>
+    )
+}
+
+export default Tab

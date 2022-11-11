ee06f51c973819add829669a554c43c0d5991d47
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-12 00:44:57 +0200

    <backend> feat: quest model

---
+++ server/models/quest.model.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose')
+
+const questSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: 'name is required',
+        maxLength: 30,
+        minLength: 3
+    },
+    description: {
+        type: String,
+        required: 'description is required',
+        minLength: 3
+    },
+    type: {
+        type: String,
+        enum: ['todo', 'daily', 'weekly', 'monthly', 'adventure'],
+        required: 'type is required'
+    },
+    due: {
+        type: Date,
+        required: 'due date is required'
+    },
+    difficulty: {
+        type: String,
+        enum: ['easy', 'medium', 'hard']
+    },
+    status: {
+        type: String,
+        enum: ['in progress', 'submitted', 'failed']
+    },
+    submitted_on: {
+        type: Date,
+    },
+    picture_submitted: {
+        type: Boolean
+    },
+    picture_url: {
+        type: String
+    }
+})
+
+const Quest = mongoose.model('Quest', questSchema)
+
+module.exports = Quest

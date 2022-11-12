119e1503b20915749d7e13af174ed6867177c929
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-12 11:43:21 +0200

    <backend> feat: connection options

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DB_URL)
+mongoose.connect(process.env.DB_URL,{
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    autoIndex: true,
+})
 .then(() => {
     console.log("database connected")
 })

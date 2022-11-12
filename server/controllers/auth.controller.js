510ad822907618d6c3f28b119539f5b8208f79bf
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-12 13:54:25 +0200

    <backend> feat: login method

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -12,12 +12,12 @@ const signUp = async (req, res) => {
 
         const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/mg
 
-        if(!passwordRegex.test(password))
+        if (!passwordRegex.test(password))
             throw 'invalid password'
 
         user.name = name
         user.username = username
-        user.password = await bcrypt.hash(password,10)
+        user.password = await bcrypt.hash(password, 10)
         user.type = type
         user.level = 1
         user.exp = 0
@@ -28,12 +28,50 @@ const signUp = async (req, res) => {
 
         res.json(user)
     }
+    catch (error) {
+        res.status(400).send(error)
+    }
+}
+
+// login method
+const login = async (req, res) => {
+
+    const { username, password } = req.body
+
+    try {
+
+        const user = await User.findOne({ username }).select("+password");
+
+        if (!user)
+            throw "Invalid Credentials";
+
+        const isMatch = await bcrypt.compare(password, user.password)
+
+        if (!isMatch)
+            throw "Invalid Credentials";
+
+        const token = jwt.sign({
+            id: user._id,
+            type: user.type
+        },
+            process.env.JWT_SECRET_KEY, {
+            expiresIn: '12h'
+        });
+
+        return res.json({
+            authorisation: {
+                token: token,
+                type: 'bearer',
+            },
+            user
+        })
+    }
     catch (error){
         res.status(400).send(error)
     }
-
 }
 
 module.exports = {
-    signUp
+    signUp,
+    login
 }

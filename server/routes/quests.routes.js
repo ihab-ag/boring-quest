34f63aae924d2ae23ed09c1276dee61db6b723f6
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-13 09:27:38 +0200

    <backend> feat: submit quest route

--- server/routes/quests.routes.js
+++ server/routes/quests.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
-const { createQuest } = require('../controllers/quests.controller')
+const { createQuest, submitQuest } = require('../controllers/quests.controller')
 const authMiddleware = require('../middleware/auth.middleware')
 
 const router = Router()
 
 router.post('/',authMiddleware, createQuest)
+
+router.put('/submit/:quest_id',authMiddleware, submitQuest)
 
 module.exports = router

a0cf2af323c06f3c8dec8672387a2171c417adad
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-13 09:24:58 +0200

    <backend> feat: submit quest

--- server/controllers/quests.controller.js
+++ server/controllers/quests.controller.js
@@ -33,11 +33,47 @@ const createQuest = async (req, res) => {
         res.json(user)
     }
     catch (error) {
-        res.status(400).send(error.message)
+        res.status(400).send(error)
     }
-
 }
 
+// submit quest
+const submitQuest = async (req, res) => {
+
+    const quest_id = req.params.quest_id
+    const user_id = req.id
+
+    try {
+
+        const quest = await Quest.findById(quest_id)
+
+        const due = new Date(quest.due)
+
+        console.log(quest_id)
+
+        const current_date = new Date()
+
+        if (due.getTime() < current_date.getTime())
+            throw 'cannot submit after due date'
+
+        quest.status = 'submitted'
+
+        await quest.save()
+
+        const user = await User.findById(user_id).populate('quests')
+
+        res.json(user)
+
+    }
+    catch (error) {
+
+        res.status(400).send(error)
+    }
+}
+
+
 module.exports = {
-    createQuest
+    createQuest,
+    submitQuest
+
 }

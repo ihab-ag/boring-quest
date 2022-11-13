a0835f6eb9075b4ee23d9792bb2d5e6a2caf915b
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-13 14:58:26 +0200

    <backend> feat( quest submit): handled level up and exp

--- server/controllers/quests.controller.js
+++ server/controllers/quests.controller.js
@@ -2,6 +2,9 @@ const Quest = require('../models/quest.model')
 const User = require('../models/user.model')
 const base64toImage = require('../utils/base64toImage')
 
+
+const current_date = new Date()
+
 // create quest
 const createQuest = async (req, res) => {
 
@@ -11,6 +14,9 @@ const createQuest = async (req, res) => {
 
     try {
 
+        
+
+
         const quest = new Quest()
 
         quest.name = name
@@ -19,6 +25,7 @@ const createQuest = async (req, res) => {
         quest.due = due
         quest.difficulty = difficulty
         quest.status = 'in progress'
+        
 
         await quest.save()
 
@@ -52,11 +59,16 @@ const submitQuest = async (req, res) => {
 
         const due = new Date(quest.due)
 
-        const current_date = new Date()
+        
 
+        // check due time
         if (due.getTime() < current_date.getTime())
             throw 'cannot submit after due date'
 
+        // check if quest is already submitted
+
+
+        // handle picture submition
         if (picture_submitted) {
 
             const path = base64toImage(base64Image, quest_id)
@@ -66,13 +78,31 @@ const submitQuest = async (req, res) => {
             quest.picture_url = path
 
         }
-
+        
         quest.status = 'submitted'
-
+        
         await quest.save()
-
+        
         const user = await User.findById(user_id).populate('quests')
+        
+        // handle exp and level
+        // gain exp based on difficulty
+        
+        if (quest.difficulty === 'easy')
+            user.exp += 5
+        else if (quest.difficulty === 'medium')
+            user.exp += 10
+        else if (quest.difficulty === 'hard')
+            user.exp += 20
+console.log(user.exp)
+        // handle level up
+        while(user.exp >= (user.level * 5)){
+            let exp_difference = user.exp - (user.level * 5)
+            user.level += 1
+            user.exp = exp_difference
+        }
 
+        await user.save()
         res.json(user)
 
     }

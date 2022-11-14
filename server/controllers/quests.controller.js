357f2542add3e6fc3165378c858c4123bc9ff133
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-14 14:22:46 +0200

    <backend> feat(submit quest): handle reoccuring quest types

--- server/controllers/quests.controller.js
+++ server/controllers/quests.controller.js
@@ -1,9 +1,11 @@
 const Quest = require('../models/quest.model')
 const User = require('../models/user.model')
 const base64toImage = require('../utils/base64toImage')
+const { incrementDay, incrementWeek, incrementMonth } = require('../utils/dateFunctions')
 
 
 const current_date = new Date()
+const REOCCURING_TYPES = ['daily', 'weekly', 'monthly']
 
 // create quest
 const createQuest = async (req, res) => {
@@ -16,7 +18,7 @@ const createQuest = async (req, res) => {
 
         const due_date = new Date(due)
 
-        if(due_date < current_date)
+        if (due_date < current_date)
             throw 'due date cannot be less than today'
 
         const quest = new Quest()
@@ -31,7 +33,7 @@ const createQuest = async (req, res) => {
 
         await quest.save()
 
-        let user = await User.findByIdAndUpdate(user_id, {
+        const user = await User.findByIdAndUpdate(user_id, {
             $push: {
                 quests: quest.id
             }
@@ -61,14 +63,14 @@ const submitQuest = async (req, res) => {
 
         const due = new Date(quest.due)
 
-        if(quest.status !== 'in progress' )
-            throw 'invalid submition status'
-
         // check due time
         if (due.getTime() < current_date.getTime())
             throw 'cannot submit after due date'
 
         // check if quest is already submitted
+
+        if (quest.status !== 'in progress')
+            throw 'invalid submition status'
 
 
         // handle picture submition
@@ -81,32 +83,66 @@ const submitQuest = async (req, res) => {
             quest.picture_url = path
 
         }
-        
+
+        quest.submitted_on = new Date()
         quest.status = 'submitted'
-        
+
         await quest.save()
-        
+
         const user = await User.findById(user_id).populate('quests')
         
         // handle exp and level
         // gain exp based on difficulty
         
         if (quest.difficulty === 'easy')
-            user.exp += 5
+        user.exp += 5
         else if (quest.difficulty === 'medium')
-            user.exp += 10
+        user.exp += 10
         else if (quest.difficulty === 'hard')
-            user.exp += 20
-
+        user.exp += 20
+        
         // handle level up
-        while(user.exp >= (user.level * 5)){
+        while (user.exp >= (user.level * 5)) {
             let exp_difference = user.exp - (user.level * 5)
             user.level += 1
             user.exp = exp_difference
         }
+        
+        // recreate quest if its reoccuring (daily, weekly, monthly)
+        if (REOCCURING_TYPES.includes(quest.type)) {
 
-        await user.save()
+            const new_quest = new Quest()
+            
+            const { name, description, type, difficulty } = quest
+
+            const iso_date = due.toISOString()
+            
+            new_quest.name = name
+            new_quest.description = description
+            new_quest.type = type
+            new_quest.created_on = due
+            new_quest.difficulty = difficulty
+            new_quest.status = 'in progress'
+            new_quest.creator = user_id
+            
+            if (type === 'daily') {
+                new_quest.due = incrementDay(iso_date)
+            }
+            else if (type === 'weekly') {
+                new_quest.due = incrementWeek(iso_date)
+            }
+            else if (type === 'monthly') {
+                new_quest.due = incrementMonth(iso_date)
+            }
+            
+            await new_quest.save()
+
+            user.quests = [...user.quests, new_quest]
+        }
+
         res.json(user)
+
+        user.save()
 
     }
     catch (error) {
@@ -117,23 +153,24 @@ const submitQuest = async (req, res) => {
 // fail quests over due date
 const failQuest = async () => {
 
-    try{
-        
-        const quests = await Quest.find({
-            'due': {$lt: current_date},
-            'status': 'in progress'})
+    try {
 
-        for(const quest of quests){
+        const quests = await Quest.find({
+            'due': { $lt: current_date },
+            'status': 'in progress'
+        })
+
+        for (const quest of quests) {
             quest.status = 'failed'
             await quest.save()
         }
 
         console.log('updated failed quests')
     }
-    catch(error){
-        console.log("failQuest error: "+ error)
+    catch (error) {
+        console.log("failQuest error: " + error)
     }
-    
+
 }
 
 module.exports = {

b69451b1b14f28ab5a1e0d6e4f81999e1881f090
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-15 14:17:16 +0200

    <backend> feat: delete friend route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express')
-const { sendOrAcceptInvite } = require('../controllers/user.controller')
+const { sendOrAcceptInvite, deleteFriend } = require('../controllers/user.controller')
 const authMiddleware = require('../middleware/auth.middleware')
 
 const router = Router()
 
 router.post('/invites/:invited_id', authMiddleware, sendOrAcceptInvite)
+
+router.delete('/companions/:invited_id', authMiddleware, deleteFriend)
 
 module.exports = router

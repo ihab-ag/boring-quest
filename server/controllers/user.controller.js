64f575aff92a49d106f5a81a1eabc2205f9efc1d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-15 14:14:06 +0200

    <backend> feat: delete friend

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -40,7 +40,39 @@ const sendOrAcceptInvite = async (req, res) => {
     }
 
 }
+// remove companion or guild
+const deleteFriend = async (req, res) => {
+
+    const user_id = req.id
+    const invited_id = req.params.invited_id
+    try {
+        // retrieve user and invited user from database
+        const [user, invited_user] = await Promise.all([
+            User.findById(user_id).exec(),
+            User.findById(invited_id).exec()
+        ])
+
+        // remove invited user from companions
+        if (user.companions.includes(invited_id)) {
+            user.companions = user.companions.filter(id => id != invited_id)
+            invited_user.companions = invited_user.companions.filter(id => id != user_id)
+
+            user.save()
+            invited_user.save()
+        }
+        else {
+            throw 'invited user not a friend'
+        }
+
+        res.json(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+
+}
 
 module.exports = {
-    sendOrAcceptInvite
+    sendOrAcceptInvite,
+    deleteFriend
 }

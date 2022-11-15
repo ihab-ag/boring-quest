4673bc9c6e36c4d0acdc8c8c1dd875bf7e692760
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-15 10:02:41 +0200

    <backend> feat( user controller): send or accept invite

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,46 @@
+const User = require('../models/user.model')
+
+// send friend invite
+const sendOrAcceptInvite = async (req, res) => {
+
+    const user_id = req.id
+    const invited_id = req.params.invited_id
+    try {
+        // retrieve user and invited user from database
+        const [user, invited_user] = await Promise.all([
+            User.findById(user_id).exec(), 
+            User.findById(invited_id).exec()
+        ])
+
+        if(user.companions.includes(invited_id)){
+            throw 'user already a companion'
+        }
+        // check if invited user invited the user
+        if (user.invites.includes(invited_id)) {
+            // remove invited user from invites
+            user.invites = user.invites.filter(id => id != invited_id)
+            // add companions to both users
+            user.companions = [...user.companions, invited_id]
+            invited_user.companions = [...invited_user.companions, user_id]
+
+            user.save()
+            invited_user.save()
+        }
+        // send invite to invited user
+        else{
+            invited_user.invites = [...invited_user.invites, user_id]
+
+            invited_user.save()
+        }
+
+        res.json(user)
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+
+}
+
+module.exports = {
+    sendOrAcceptInvite
+}

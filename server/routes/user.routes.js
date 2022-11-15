a19362891e2de3ae99d45fc2004adff92dd763a0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-15 20:00:55 +0200

    <backend> feat: search users route

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express')
-const { sendOrAcceptInvite, deleteFriend } = require('../controllers/user.controller')
+const { sendOrAcceptInvite, deleteFriend, searchUsers } = require('../controllers/user.controller')
 const authMiddleware = require('../middleware/auth.middleware')
 
 const router = Router()
@@ -7,5 +7,7 @@ const router = Router()
 router.post('/invites/:invited_id', authMiddleware, sendOrAcceptInvite)
 
 router.delete('/friends/:deleted_id', authMiddleware, deleteFriend)
+
+router.get('/search/:username', authMiddleware, searchUsers)
 
 module.exports = router

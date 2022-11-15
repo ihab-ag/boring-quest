038dde7afd1ca75e46aae682795c4874e9451330
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-15 09:57:49 +0200

    <backend> fix: minute rule

--- server/services/failQuestsScheduler.js
+++ server/services/failQuestsScheduler.js
@@ -3,6 +3,7 @@ const { failQuest } = require('../controllers/quests.controller')
 
 const rule = new schedule.RecurrenceRule()
 rule.hour = 0
+rule.minute = 0
 
 const failQuestsScheduler = schedule.scheduleJob(rule, failQuest)
 

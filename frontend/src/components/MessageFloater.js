891caa1079aa8d9b373cb39bee8e848b7c3c1171
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-16 19:08:50 +0200

    <frontend> feat: login error message

--- frontend/src/components/MessageFloater.js
+++ frontend/src/components/MessageFloater.js
@@ -1,36 +1,45 @@
-import { View} from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
-import { useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Animated, { useAnimatedStyle, useSharedValue, withDelay, withRepeat, withTiming } from 'react-native-reanimated'
 import { useEffect } from 'react'
+import { setMessage } from '../redux/slices/globalMessageSlice'
 
 const MessageFloater = () => {
-    
+
     const { status } = useSelector(state => state.globalMessage)
 
+    const dispatch = useDispatch()
+
     const position = useSharedValue(-50)
+    const display = useSharedValue('none')
 
     const config = {
         duration: 500
     }
 
     useEffect(() => {
-            position.value = withTiming(0, config,()=>{
-                position.value = withDelay(1000,withTiming(-50,config))
-            })
+        display.value = 'flex'
+        position.value = withTiming(0, config, () => {
+            position.value = withDelay(1000, withTiming(-50, config, () => {
+                display.value = 'none'
+            }))
+        })
     },[status])
 
 
     const viewStyle = useAnimatedStyle(() => {
         return {
-            transform: [{ translateY: position.value
-             }],
+            transform: [{
+                translateY: position.value
+            }],
+            display: display.value
         }
     })
 
     const messageOutput = {
-        good: {
-            message: "All good and nothing to do",
+        'login error': {
+            message: "Invalid Credentials",
             color: "bg-hard"
         },
         welcome: {
@@ -42,11 +51,12 @@ const MessageFloater = () => {
             color: "bg-easy"
         },
     }
+     
     return (
         <View className='justify-center items-center absolute w-full my-6'>
-                <Animated.View className={`justify-center w-fit items-center ${messageOutput[status].color} p-2 rounded`} style={viewStyle}>
-                    <Animated.Text className='font-inter-medium size-sm text-secondary/90'>{messageOutput[status].message}</Animated.Text>
-                </Animated.View>
+            <Animated.View className={`justify-center w-fit items-center ${messageOutput[status]?.color} p-2 rounded`} style={viewStyle}>
+                <Animated.Text className='font-inter-medium size-sm text-secondary/90'>{messageOutput[status]?.message}</Animated.Text>
+            </Animated.View>
         </View>
 
     )

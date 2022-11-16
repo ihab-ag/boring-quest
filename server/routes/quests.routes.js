258462a0e3289c509690d5609d3387c339f0ba00
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-16 09:14:10 +0200

    <backend> feat(submit quest route): adventurer middleware

--- server/routes/quests.routes.js
+++ server/routes/quests.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require('express')
 const { createQuest, submitQuest } = require('../controllers/quests.controller')
+const adventurerMiddleware = require('../middleware/adventurer.middleware')
 const authMiddleware = require('../middleware/auth.middleware')
 
 const router = Router()
 
 router.post('/',authMiddleware, createQuest)
 
-router.put('/submit/:quest_id',authMiddleware, submitQuest)
+router.put('/submit/:quest_id',authMiddleware,adventurerMiddleware, submitQuest)
 
 module.exports = router

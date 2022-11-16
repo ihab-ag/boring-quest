ad70b198543c3b44450f598324819758b1eb28ff
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-16 20:45:08 +0200

    <frontend> fix: set user

--- frontend/src/redux/slices/userSlice.js
+++ frontend/src/redux/slices/userSlice.js
@@ -2,10 +2,21 @@ import { createSlice } from '@reduxjs/toolkit'
 
 export const userSlice = createSlice({
     name: 'user',
-    initialState:{},
-    reducers:{
-
+    initialState: {},
+    reducers: {
+        setUser: (state, action) => {
+            state.id= action.payload.id,
+            state.name= action.payload.name,
+            state.username= action.payload.username,
+            state.type= action.payload.type,
+            state.level= action.payload.level,
+            state.exp= action.payload.exp,
+            state.deaths= action.payload.deaths,
+            state.health= action.payload.health
+        }
     }
 })
 
-export default dateSlice.reducer
+export const { setUser } = userSlice.actions
+
+export default userSlice.reducer

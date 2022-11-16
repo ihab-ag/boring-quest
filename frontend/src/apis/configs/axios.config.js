da5d871ae4664e7cc4682147add20c30725b0a38
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-16 19:02:15 +0200

    <frontend> fix: base url

--- frontend/src/apis/configs/axios.config.js
+++ frontend/src/apis/configs/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import store from "../../redux/store"
 
-const BASE_URL = '192.168.44.177:8000/'
+const BASE_URL = 'http://192.168.44.177:8000/'
 
 const TOKEN = store.getState().auth.token
 

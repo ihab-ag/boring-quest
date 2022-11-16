dc849bf1ef7027aa502ceda1a289c43e52d379a1
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-16 20:46:57 +0200

    <frontend> feat: user reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,12 +3,14 @@ import dateReducer from './slices/dateSlice'
 import adventureReducer from './slices/adventureSlice'
 import globalMessageReducer from './slices/globalMessageSlice'
 import authReducer from './slices/authSlice'
+import userReducer from './slices/userSlice'
 
 export default store = configureStore({
     reducer: {
         date: dateReducer,
         adventure: adventureReducer,
         globalMessage: globalMessageReducer,
-        auth: authReducer
+        auth: authReducer,
+        user: userReducer
     }
 })

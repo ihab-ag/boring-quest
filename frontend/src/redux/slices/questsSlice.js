3c718d511fa61e11272a007cc2257be030331c04
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-17 17:15:37 +0200

    <frontend> feat: set quests reducer

--- frontend/src/redux/slices/questsSlice.js
+++ frontend/src/redux/slices/questsSlice.js
@@ -1,11 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 export const questsSlice = createSlice({
-    name:'quests',
-    initialState:{},
-    reducers:{
-
+    name: 'quests',
+    initialState: {},
+    reducers: {
+        setQuests: (state, action) => {
+            state.array = action.payload
+        }
     }
 })
 
+export const { setQuests } = questsSlice.actions
 export default questsSlice.reducer

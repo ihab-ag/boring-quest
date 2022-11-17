12386c6c48973c33b84d0ad821aa2d28dc4f7da9
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-17 17:25:18 +0200

    <frontend> feat: dynamic data

--- frontend/src/components/QuestsSection.js
+++ frontend/src/components/QuestsSection.js
@@ -3,33 +3,20 @@ import React, { useState } from 'react'
 import SectionTitleText from './SectionTitleText'
 import ProgressBar from './ProgressBar'
 import mapQuests from '../helpers/mapQuests'
-import { useDispatch } from 'react-redux'
-import { setMessage } from '../redux/slices/globalMessageSlice'
 
-const QuestsSection = ({navigation}) => {
+const QuestsSection = ({ navigation, quests, name }) => {
 
-    const dispatch = useDispatch()
-
-    const [quests, setQuests] = useState([
-        {id:1, title: 'dummy1', description: 'dummy desc', status:'in progress', difficulty: 'easy', date:new Date()},
-        {id:2, title: 'dummy2', description: 'dummy desc something to get larger text bla shsg dhakd', status:'submitted', difficulty: 'hard', date:new Date()},
-        {id:3, title: 'dummy3', description: 'dummy desc', status:'failed', difficulty: 'medium', date:new Date()},
-        {id:4, title: 'dummy3', description: 'dummy desc', status:'in progress', difficulty: 'hard', date:new Date()},
-    ])
+    const submitted_quests = quests.filter((quest) => quest.status === 'submitted')
 
     return (
         <View className='mt-3 flex-1'>
             <View className='mb-4'>
-                <SectionTitleText>Daily</SectionTitleText>
-                <ProgressBar bg_color='bg-easy' />
+                <SectionTitleText>{name}</SectionTitleText>
+                <ProgressBar bg_color='bg-easy' value={submitted_quests.length} total={quests.length} />
             </View>
-            <ScrollView nestedScrollEnabled={true} showsVerticalScrollIndicator={false} contentContainerStyle={{flex:1}}>
-            { quests.length > 0 ? mapQuests(quests, (key, quest)=> {
-            navigation.navigate('Quest', {quest})
-            dispatch(setMessage('bad'))
-            }) :
-            (<Text> Empty State</Text>)}
-            </ScrollView>
+            {mapQuests(quests, (key, quest) => {
+                navigation.navigate('Quest', { quest })
+            })}
         </View>
     )
 }

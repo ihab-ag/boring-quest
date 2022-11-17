ea9c00b9bef59ffc572109df313077613e140ae1
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-17 10:15:30 +0200

    <frontend> feat: quests slice

---
+++ frontend/src/redux/slices/questsSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const questsSlice = createSlice({
+    name:'quests',
+    initialState:{},
+    reducers:{
+
+    }
+})
+
+export default questsSlice.reducer

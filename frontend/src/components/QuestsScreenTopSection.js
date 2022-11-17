c46ccec2501cdf5619155b02bd0fcf5d34fc5022
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-17 10:13:45 +0200

    <frontend> feat: dynamic user info

--- frontend/src/components/QuestsScreenTopSection.js
+++ frontend/src/components/QuestsScreenTopSection.js
@@ -3,8 +3,14 @@ import React from 'react'
 import Avatar from '../components/Avatar'
 import ProgressBar from './ProgressBar'
 import TopColoredSection from '../layouts/TopColoredSection'
+import { useSelector } from 'react-redux'
 
 const QuestsScreenTopSection = () => {
+    const user = useSelector(state => state.user)
+    const health = user.health
+    const level = user.level
+    const exp = user.exp
+    const total_exp = level * 5
     return (
         <TopColoredSection color='bg-primary' flex='flex-row'>
             <Avatar />
@@ -12,16 +18,16 @@ const QuestsScreenTopSection = () => {
                 <View>
                     <View className='flex-row justify-between items-end'>
                         <Text className='font-inter-medium text-white text-base'>Health</Text>
-                        <Text className='font-inter text-white text-sm'>100/300</Text>
+                        <Text className='font-inter text-white text-sm'>{health}/1000</Text>
                     </View>
-                    <ProgressBar bg_color='bg-hard' />
+                    <ProgressBar bg_color='bg-hard' value={health} total={1000}/>
                 </View>
                 <View>
                     <View className='flex-row justify-between items-end'>
-                        <Text className='font-inter-medium text-white text-base'>lv. 16</Text>
-                        <Text className='font-inter text-white text-sm'>100/300</Text>
+                        <Text className='font-inter-medium text-white text-base'>lv. {level}</Text>
+                        <Text className='font-inter text-white text-sm'>{exp}/{total_exp}</Text>
                     </View>
-                    <ProgressBar bg_color='bg-medium' />
+                    <ProgressBar bg_color='bg-medium' value={exp} total={total_exp} />
                 </View>
             </View>
         </TopColoredSection>

ea0643c04f40091ab14f670a15a1a7a763b14bd1
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-17 10:07:34 +0200

    <frontend> feat: progress bar dynamic width

--- frontend/src/components/ProgressBar.js
+++ frontend/src/components/ProgressBar.js
@@ -1,10 +1,11 @@
 import { View } from 'react-native'
+import { convertValueToProgressbarWidth } from '../helpers/convertValueForProgressbarWidth'
 
-const ProgressBar = ({ bg_color }) => {
-    
+const ProgressBar = ({ bg_color, value, total }) => {
+    const width = convertValueToProgressbarWidth(value,total)
     return (
         <View className={`h-4 bg-white mt-0.5 border-primary border-2 rounded-sm border-solid`}>
-            <View className={`flex-1 ${bg_color} w-2/5`} />
+            <View className={`flex-1 ${bg_color} ${width}`} />
         </View>
     )
 }

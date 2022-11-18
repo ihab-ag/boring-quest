7c294827f0d6997bbf53e74df7800fec57e3151e
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 12:13:47 +0200

    <frontend> fix: name. due values

--- frontend/src/forms/validation/adventureValidation.js
+++ frontend/src/forms/validation/adventureValidation.js
@@ -2,14 +2,14 @@ import * as yup from 'yup'
 import { currentDay } from '../../helpers/currentDate'
 
 const adventureValidationSchema = yup.object({
-    title: yup.string()
+    name: yup.string()
         .required()
         .min(3)
         .max(20),
     description: yup.string()
         .required()
         .min(3),
-    date: yup.date()
+    due: yup.date()
     .min(new Date(currentDay -1 )),
     quests: yup.array()
     .test({

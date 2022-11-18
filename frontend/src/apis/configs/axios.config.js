7f257f51f4c292754adaf3508c9acad94428c0d0
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 12:19:06 +0200

    <frontend> feat: put request

--- frontend/src/apis/configs/axios.config.js
+++ frontend/src/apis/configs/axios.config.js
@@ -3,18 +3,28 @@ import store from "../../redux/store"
 
 const BASE_URL = 'http://192.168.44.177:8000/'
 
-const TOKEN = store.getState().auth.token
+export const TOKEN = store.getState().auth.token
 
 const HEADERS = {
     headers: {
         'Authorization': `Bearer ${TOKEN}`
     }
 }
+console.log(HEADERS)
 
 export const axiosPostReq = async (route, data) => {
     try {
         return await axios.post(BASE_URL + route, data, HEADERS)
     } catch (error) {
-        return error;
+        return error
     }
 }
+
+export const axiosPutReq = async (route, data) => {
+    try {
+        return await axios.put(BASE_URL + route, data, HEADERS)
+    } catch (error) {
+        return error
+    }
+}
+

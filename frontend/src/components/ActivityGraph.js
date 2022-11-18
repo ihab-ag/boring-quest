77f65a97f4d5dfacd2e35549df6006dd6aa8af4b
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 15:29:46 +0200

    <frontend> feat: make graph function

--- frontend/src/components/ActivityGraph.js
+++ frontend/src/components/ActivityGraph.js
@@ -1,12 +1,36 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
+const GRAPH_HEIGHT = 400
+const GRAPH_WIDTH = 370
+
+const makeGraph = (data) => {
+    const max = Math.max(...data.map(val => val.value))
+    const min = Math.min(...data.map(val => val.value))
+    const y = scaleLinear().domain([0, max]).range([GRAPH_HEIGHT, 35])
+  
+    const x = scaleTime()
+      .domain([1, 15])
+      .range([10, GRAPH_WIDTH - 10])
+  
+    const curvedLine = line()
+      .x(d => x(new Date(d.date)))
+      .y(d => y(d.value))
+      .curve(curveBasis)(data)
+  
+    const skPath = Skia.Path.MakeFromSVGString(curvedLine)
+  
+    return {
+      max,
+      min,
+      curve: skPath,
+    }
+  }
+  
 const ActivityGraph = () => {
 
-    const GRAPH_HEIGHT = 400
-    const GRAPH_WIDTH = 370
 
-    
+
     return (
         <View>
             <Text>ActivityGraph</Text>

33e525c177b4f33092400f3b56c37362e168bc01
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 15:28:15 +0200

    <frontend> feat: graph dimensions

---
+++ frontend/src/components/ActivityGraph.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ActivityGraph = () => {
+
+    const GRAPH_HEIGHT = 400
+    const GRAPH_WIDTH = 370
+
+    
+    return (
+        <View>
+            <Text>ActivityGraph</Text>
+        </View>
+    )
+}
+
+export default ActivityGraph

392ead3557422461bbdd414feffbabd87ffba08c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 12:17:16 +0200

    <frontend> fat: map quest sections

--- frontend/src/components/QuestsSections.js
+++ frontend/src/components/QuestsSections.js
@@ -4,14 +4,14 @@ import QuestsSection from './QuestsSection'
 import DatePickerSection from './DatePickerSection'
 
 const QuestsSections = ({ navigation, quests }) => {
-    
+
     return (
         <DefaultScreen>
             <DatePickerSection />
             <>
-                {   
-                    Object.entries(quests).map((quest) => 
-                        quest[1].length > 0 && <QuestsSection navigation={navigation} name={quest.shift()} quests={quest[0]} />
+                {
+                    Object.entries(quests).map((quest, key) =>
+                        quest[1].length > 0 && <QuestsSection key={key} navigation={navigation} name={quest.shift()} quests={quest[0]} />
                     )
 
                 }

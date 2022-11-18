bd8703d7e42fab34570b0f637a1f41db25ace50d
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 12:12:14 +0200

    <frontend> feat: cleaned code

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,10 +1,8 @@
-import { View, Text, TouchableOpacity, BackHandler } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import AnimatedBackground from '../components/AnimatedBackground'
 import LoginFormLayout from '../layouts/LoginFormLayout'
 import LoginForm from '../forms/LoginForm'
-import { useEffect } from 'react'
 
 const LoginScreen = ({ navigation, route }) => {
 

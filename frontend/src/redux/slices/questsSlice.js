c525268c8ee6b196d5d7b919bde8c01539533627
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-18 12:12:32 +0200

    <frontend> feat: set fetched quest

--- frontend/src/redux/slices/questsSlice.js
+++ frontend/src/redux/slices/questsSlice.js
@@ -1,14 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+const initialState = {
+    fetched: null
+}
 export const questsSlice = createSlice({
     name: 'quests',
     initialState: {},
     reducers: {
         setQuests: (state, action) => {
             state.array = action.payload
+        },
+        setFetchedQuest: (state, action) => {
+            state.array = [...state.array,action.payload]
         }
     }
 })
 
-export const { setQuests } = questsSlice.actions
+export const { setQuests, setFetchedQuest } = questsSlice.actions
 export default questsSlice.reducer

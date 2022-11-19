bbf1bafaa37848894f5c8694dd388508e79c86a4
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-19 14:55:47 +0200

    <frontend> feat: expo notifications configuration

---
+++ frontend/src/components/AppNotifications.js
@@ -0,0 +1,41 @@
+import * as Notifications from 'expo-notifications'
+import { useEffect } from 'react';
+
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true
+    }),
+  })
+  
+const AppNotifications = () => {
+
+
+    async function registerForPushNotificationsAsync() {
+        let token;
+
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            alert('Failed to get push token for push notification!');
+            return;
+        }
+        token = (await Notifications.getExpoPushTokenAsync()).data;
+        console.log(token);
+
+        return token;
+    }
+        useEffect(() => {
+            registerForPushNotificationsAsync();
+        }, []);
+        return (
+            <></>
+        )
+}
+
+        export default AppNotifications

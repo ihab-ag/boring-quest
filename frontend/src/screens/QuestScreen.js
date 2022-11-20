7f74281bb04abd3c18d6c00ef2c0975c9132495b
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-20 13:56:43 +0200

    <frontend> feat: handle quest status

--- frontend/src/screens/QuestScreen.js
+++ frontend/src/screens/QuestScreen.js
@@ -9,8 +9,10 @@ import { Feather } from '@expo/vector-icons';
 import FullWidthButton from '../components/FullWidthButton'
 import { putSubmit } from '../apis/putSubmit.api'
 import { useDispatch } from 'react-redux'
-import { setFetchedQuest } from '../redux/slices/questsSlice'
+import { addQuest } from '../redux/slices/questsSlice'
 import mapQuests from '../helpers/mapQuests'
+import { setUser } from '../redux/slices/userSlice'
+import { destructureUser } from '../helpers/destructureUser'
 
 const QuestScreen = ({ navigation, route }) => {
 
@@ -21,17 +23,20 @@ const QuestScreen = ({ navigation, route }) => {
 
     const due_date = new Date(quest.due).toDateString()
     route.name = quest.name
-    // const url = `../../../server/${quest.picture_url}`
-    // console.log(url)
+
     const dispatch = useDispatch()
-    console.log(quest)
+
     const handleSubmit = async () => {
         const res = await putSubmit(quest._id, {
             "picture_submitted": true,
             "base64Image": photo.base64
         })
-        if(res.status === 200){
-            dispatch(setFetchedQuest(res.data))
+        console.log(res)
+        if (res.status === 200) {
+            dispatch(addQuest(res.data.quest))
+            dispatch(setUser(destructureUser(res.data.user)))
+            console.log(res.data)
+            navigation.goBack()
         }
     }
 
@@ -47,7 +52,7 @@ const QuestScreen = ({ navigation, route }) => {
                     <Text className='font-inter-semibold text-secondary text-xl'>Due:</Text>
                     <Text className='font-inter-medium text-secondary text-base'>{due_date}</Text>
                 </View>
-                { quest.status === 'in progress' && <View className='mt-4 flex-row justify-between items-start'>
+                {quest.status === 'in progress' && <View className='mt-4 flex-row justify-between items-start'>
                     <Text className='font-inter-semibold text-secondary text-xl'>Submission:</Text>
                     <View className='gap-y-4'>
                         <TouchableOpacity onPress={() => setCameraVisible(true)}>
@@ -62,23 +67,26 @@ const QuestScreen = ({ navigation, route }) => {
                     </View>
                 )}
                 {
-                quest.picture_url && (
-                    <View className='border-2 border-primary mt-4'>
-                        <Image className='align-super h-96' source={{uri:'http://192.168.44.177:8000/63772f3ff37e1ef328f84e0d.jpg'}
-                    } />
-                    </View>
-                )}
+                    quest.picture_url && (
+                        <View className='border-2 border-primary mt-4'>
+                            <Image className='align-super h-96' source={{ uri: 'http://192.168.1.101:8000/' + quest._id + '.jpg' }
+                            } />
+                        </View>
+                    )}
                 {
                     quest.quests && (
-                        <View>
-                        {mapQuests(quest.quests, (key, quest) => {
-                            navigation.navigate('Quest', { quest })
-                        })}
+                        <View className='mt-4'>
+                            <Text className='font-inter-semibold text-secondary text-xl mb-4'>Quests:</Text>
+                            {mapQuests(quest.quests, (key, quest) => {
+                                navigation.navigate('Quest', { quest })
+                            })}
                         </View>
                     )
                 }
             </DefaultScreen>
-            <FullWidthButton title='SUBMIT' onPress={handleSubmit} />
+            {quest.status === 'in progress' &&
+
+                <FullWidthButton title='SUBMIT' onPress={handleSubmit} />}
             {cameraVisibile &&
                 <MyCamera setPhoto={setPhoto} setVisible={setCameraVisible} />
             }

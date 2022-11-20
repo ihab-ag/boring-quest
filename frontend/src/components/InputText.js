e5ea082ba8b81fa296f6ad4193e55696d7373833
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-20 12:42:00 +0200

    <frontend> feat: login variant

--- frontend/src/components/InputText.js
+++ frontend/src/components/InputText.js
@@ -1,10 +1,10 @@
 import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
-const InputText = ({ placeholder, numberOfLines, onChangeText, onBlur, value, password = false }) => {
+const InputText = ({ placeholder, numberOfLines, onChangeText, onBlur, value, password = false, login = false }) => {
     return (
         <TextInput
-            className='px-2 pt-1 border-2 border-secondary text-secondary rounded-sm mt-2 w-full bg-white placeholder:text-xl'
+            className={`px-2 pt-1 border-2 border-secondary text-secondary rounded-sm mt-2 w-full bg-white placeholder:text-xl ${login && 'border-0 border-b-2 border-primary'}`}
             placeholder={placeholder}
             textAlignVertical='top'
             cursorColor='#118AB2'

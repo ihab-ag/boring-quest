b531f7ac6fcf5585c0347ef01c8d333679b0a387
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-23 22:54:16 +0200

    <frontend> fix: font weight

--- frontend/src/components/FullWidthButton.js
+++ frontend/src/components/FullWidthButton.js
@@ -4,7 +4,7 @@ import React from 'react'
 const FullWidthButton = ({ title, onPress}) => {
     return (
         <TouchableOpacity onPress={onPress} className='p-4 w-full bg-primary items-center justify-center' >
-            <Text className='font-inter-semibold text-white text-lg'>{title}</Text>
+            <Text className='font-inter-bold text-white text-lg'>{title}</Text>
         </TouchableOpacity >
     )
 }

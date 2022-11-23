3e6a10c3015787c55baa782af20bcb97c9bec0af
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-23 22:49:47 +0200

    <frontend> feat: avatar

--- frontend/src/components/DrawerTopSection.js
+++ frontend/src/components/DrawerTopSection.js
@@ -7,10 +7,11 @@ const DrawerTopSection = () => {
     const user = useSelector( state => state.user)
     const name = user.name
     const username = user.username
+    const avatar = user.avatar
     return (
         <View className='flex-row px-3 py-5 mb-5 bg-primary items-center justify-start gap-x-7'>
             <View className='border-medium border-2 rounded-sm border-solid'>
-                <Avatar/>
+                <Avatar avatar={avatar} />
             </View>
             <View className='flex-1 border-2 border-l-0 pr-3 border-solid border-medium h-20 justify-center'>
                 <Text className='font-inter-semibold text-white text-base m-0 tracking-wider capitalize'>{name}</Text>

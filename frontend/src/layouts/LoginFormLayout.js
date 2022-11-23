91d0016cf4ea6de7115058ee08de5c80f7966b41
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-24 00:00:11 +0200

    <frontend> feat: design change

--- frontend/src/layouts/LoginFormLayout.js
+++ frontend/src/layouts/LoginFormLayout.js
@@ -4,13 +4,11 @@ import { ScrollView } from 'react-native-gesture-handler'
 
 const LoginFormLayout = ({ route, children }) => {
     return (
-        <View className='bg-white w-full h-2/3 p-1 pb-0 rounded-t-2xl'>
-            <Text className='text-2xl text-primary font-spaceMono-bold px-6 py-1' >{route.name}</Text>
-            <View className='bg-white border-4 p-2 border-primary border-b-0 flex-1 rounded-t-2xl'>
-                <ScrollView showsVerticalScrollIndicator={false}>
+        <View className='bg-white w-full h-2/3 p-4 pb-0 rounded-t-2xl border-2 border-primary'>
+            <Text className='text-2xl text-primary font-spaceMono-bold px-6 py-1 text-center' >{route.name}</Text>
+                <ScrollView showsVerticalScrollIndicator={false} >
                     {children}
                 </ScrollView>
-            </View>
         </View>
     )
 }

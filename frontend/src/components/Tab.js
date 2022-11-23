cdf00753e8c1718f731493b9d169f044ce9eb435
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-23 23:29:47 +0200

    <frontend> fix: moved to components folder

--- frontend/src/components/Tab.js
+++ frontend/src/components/Tab.js
@@ -7,7 +7,7 @@ const Tab = ({ onPress, title, selected=false }) => {
         <View className='flex-1'>
         <TouchableOpacity
             onPress={onPress}
-            className={`border-2 justify-center items-center ${ selected ? 'border-white bg-primary' : 'bg-white border-primary'} py-2 rounded`} >
+            className={`border-2 justify-center items-center ${ selected ? 'border-white bg-primary' : 'bg-white border-primary'} py-1 rounded`} >
             <Text className={`font-inter-semibold text-base ${ selected ? 'text-white' : 'text-primary'}`}>{title}</Text>
         </TouchableOpacity>
         </View>

88d8766bad9c9af136dc3bd040ee0ab3ef4c50f4
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-24 00:02:24 +0200

    <frontend> feat: set data

--- frontend/src/redux/slices/questsSlice.js
+++ frontend/src/redux/slices/questsSlice.js
@@ -1,21 +1,35 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { afternoon, midnight, morning, night } from '../../constants/Data'
 import { mapQuestToDays } from '../../helpers/mapQuestToDays'
 
 const initialState = {
-    fetched: null
+    midnight: midnight,
+    morning: morning,
+    afternoon: afternoon,
+    night: night,
+    total_quests: 0,
+    total_submitted: 0
 }
 export const questsSlice = createSlice({
     name: 'quests',
-    initialState: {},
+    initialState,
     reducers: {
         setQuests: (state, action) => {
             state.map = action.payload
         },
         addQuest: (state, action) => {
             state.map = mapQuestToDays(state.map, action.payload)
+        },
+        setData: (state, action) => {
+            state.midnight = action.payload[0]
+            state.morning = action.payload[1]
+            state.afternoon = action.payload[2]
+            state.night = action.payload[3]
+            state.total_quests = action.payload[4]
+            state.total_submitted = action.payload[5]
         }
     }
 })
 
-export const { setQuests, addQuest, } = questsSlice.actions
+export const { setQuests, addQuest, setData } = questsSlice.actions
 export default questsSlice.reducer

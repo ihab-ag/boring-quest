e4bbece89a1f94c6a7a83ea97483625864c522da
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-23 22:54:46 +0200

    <frontend> fix: text size

--- frontend/src/components/InputText.js
+++ frontend/src/components/InputText.js
@@ -4,7 +4,7 @@ import React from 'react'
 const InputText = ({ placeholder, numberOfLines, onChangeText, onBlur, value, password = false, login = false }) => {
     return (
         <TextInput
-            className={`px-2 pt-1 border-2 border-secondary text-secondary rounded-sm mt-2 w-full bg-white placeholder:text-xl ${login && 'border-0 border-b-2 border-primary'}`}
+            className={`px-2 pt-1 border-2 border-secondary text-secondary rounded-sm mt-2 w-full bg-white placeholder:text-lg ${login && 'border-0 border-b-2 border-primary'}`}
             placeholder={placeholder}
             textAlignVertical='top'
             cursorColor='#118AB2'

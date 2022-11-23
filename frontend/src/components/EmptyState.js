220774f0430a2a0da335a73478ba0342b7dd061c
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-23 22:50:51 +0200

    <frontend> feat: empty state

---
+++ frontend/src/components/EmptyState.js
@@ -0,0 +1,13 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+const EmptyState = () => {
+    return (
+        <View className='bg-primary items-center rounded-xl mt-2 flex-row justify-between px-4 border-2 border-medium'>
+            <Text className='text-white font-inter-medium text-lg' >Nothing to see here !</Text>
+            <Image className='h-20 w-20' resizeMode='cover' source={require('../../assets/test_avatars/empty_state.gif')} />
+        </View>
+    )
+}
+
+export default EmptyState

143b4fd5996f7cb32af7810e8dffffd64cb3feb8
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-23 22:43:50 +0200

    <frontend> feat: user avatar uri

--- frontend/src/components/Avatar.js
+++ frontend/src/components/Avatar.js
@@ -1,9 +1,10 @@
 import { Image } from 'react-native'
 import React from 'react'
 
-const Avatar = () => {
+const Avatar = ({avatar}) => {
+
   return (
-    <Image className='h-20 w-20'  source={require('../../assets/test_avatars/avatar.png')} />
+    <Image className='h-20 w-20'  source={{ uri: 'http://192.168.1.102:8000/' + avatar }} />
   )
 }
 export default Avatar

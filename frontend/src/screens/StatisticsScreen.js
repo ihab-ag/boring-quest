a86503bc2f0af65b968717ecacba72721dcfbc40
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-24 10:37:40 +0200

    <frontend> feat: statistics screen

--- frontend/src/screens/StatisticsScreen.js
+++ frontend/src/screens/StatisticsScreen.js
@@ -1,15 +1,22 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
+import { useSelector } from 'react-redux'
 import ActivityGraph from '../components/ActivityGraph'
+import BarGraph from '../components/BarGraph'
 import CustomHeader from '../components/CustomHeader'
+import StatsCard from '../components/StatsCard'
 import DefaultScreen from '../layouts/DefaultScreen'
 
 const StatisticsScreen = ({navigation, route}) => {
+  const user = useSelector(state => state.user)
+  const quests = useSelector(state => state.quests)
   return (
     <>
         <CustomHeader navigation={navigation} route={route} type='drawer' />
         <DefaultScreen>
-            <ActivityGraph />
+            <StatsCard user={user} quests={quests} />
+            <ActivityGraph name='Productivity'/>
+            <BarGraph />
         </DefaultScreen>
     </>
   )

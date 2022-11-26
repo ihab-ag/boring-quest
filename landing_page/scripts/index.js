6a5505a015d97f82abbeaefbac7bf824bcecf7a5
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-11-26 15:17:47 +0200

    <landingpage> feat: title animation

---
+++ landing_page/scripts/index.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', function (e) {
+
+    const title = document.querySelectorAll('.title')
+
+    // title animation
+    let index = 0, length = title.length
+    for (index; index < length; index++) {
+        const pos = window.pageYOffset * title[index].dataset.rate
+
+        title[index].style.transform = 'translate3d(' + pos + 'px, 0px,  0px)'
+    }
+})
